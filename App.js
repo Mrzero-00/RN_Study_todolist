29b8a341a68b50f5b230647203866eaa431aeccb
son <son@sonui-MacBookAir.local>
2022-03-08 11:25:39 +0900

    인풋데이터에서 키보드입력 받기

--- App.js
+++ App.js
@@ -1,11 +1,66 @@
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity , TextInput} from 'react-native';
 import { useState } from 'react';
 
 export default function App() {
   const [currentPage , setCurrentPage] = useState("work");
+  const [inputText,setInputText] = useState(""); 
+  const [todoAddState,setTodoAddState] = useState(false); 
   const onPressLogic = (e)=>{
     setCurrentPage(e);
   }
+  const onChangeLogic = ()=>{
+    console.log(e);
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor:currentPage==="work"?"orange":'#000',
+      paddingHorizontal:30
+    },
+    btnContainer:{
+      flex:1,
+      flexDirection:"row",
+      marginTop:100,
+      justifyContent:"space-between",
+    },
+    btnText:{
+      fontSize:38,
+      fontWeight:"600",
+      color:"#fff"
+    },
+    pageTitleContainer:{
+      flex:7
+    },
+    pageTitleText:{
+      fontSize:28,
+      fontWeight:"600",
+      color:"#fff"
+    },
+    inputText:{
+      marginTop:10,
+      backgroundColor:"#fff",
+      borderRadius:50,
+      paddingHorizontal:10,
+      paddingVertical:10,
+      fontSize:16
+    },
+    addBtnContainer:{
+      flex:2,
+      alignItems:"center",
+      justifyContent:"center"
+    },
+    addBtn:{
+      paddingHorizontal:12,
+      borderRadius:50,
+      backgroundColor:"#fff",
+      justifyContent:"center",
+      alignItems:"center"
+    },
+    addBtnText:{
+      fontSize:36,
+    }
+  });
   return (
     <View style={styles.container}>
       <View style={styles.btnContainer}>
@@ -18,34 +73,26 @@ export default function App() {
       </View>
       <View style={styles.pageTitleContainer}>
         <Text style={styles.pageTitleText}>{currentPage}</Text>
+        {todoAddState&&<TextInput 
+          style={styles.inputText} 
+          onChangeText={setInputText} 
+          value={inputText}>
+        </TextInput>}
+      </View>
+      <View style={styles.addBtnContainer}>
+        <TouchableOpacity 
+          style={styles.addBtn}
+          onPress={()=>{
+            setInputText("");
+            setTodoAddState(!todoAddState)
+          }}>
+          <Text style={styles.addBtnText}>
+           {todoAddState?"x":"+"}
+          </Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    paddingHorizontal:30
-  },
-  btnContainer:{
-    flex:1,
-    flexDirection:"row",
-    marginTop:100,
-    justifyContent:"space-between",
-  },
-  btnText:{
-    fontSize:38,
-    fontWeight:"600",
-    color:"#fff"
-  },
-  pageTitleContainer:{
-    flex:9
-  },
-  pageTitleText:{
-    fontSize:28,
-    fontWeight:"600",
-    color:"#fff"
-  }
-});
+
